8cc1cf6119a4de9a8335dd0fde7261945b9391da
Raz Zinger <raz.roman-zinger@hp.com>
2025-03-18 22:59:19 +0200

    ddd

---
+++ terra-dry-run/sqs_s3 copy.tf
@@ -0,0 +1,11 @@
+resource "aws_sqs_queue" "my_queue" {
+  name = var.sqs_name
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.terraform_bucket_name
+}
