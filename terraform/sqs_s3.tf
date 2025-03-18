73f19f004c54c9040d1ee6c94a670d8c30305c1d
Raz Zinger <raz.roman-zinger@hp.com>
2025-03-19 00:22:16 +0200

    ff

--- terraform/sqs_s3.tf
+++ terraform/sqs_s3.tf
@@ -4,10 +4,10 @@ resource "aws_sqs_queue" "my_queue" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
-  region = var.aws_region
+  acl = "private"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.terraform_bucket_name
-  region = var.aws_region
+  acl = "private"
 }
