db9139db008cca724c47bbc6c5e065ca864b7680
Raz Zinger <raz.roman-zinger@hp.com>
2025-03-19 00:09:47 +0200

    ff

--- terra-2/output.tf
+++
@@ -1,3 +0,0 @@
-output "elb_dns_name" {
-  value = aws_elb.classic_elb.dns_name
-}
--- terra-dry-run/sqs_s3.tf
+++ terra-dry-run/sqs_s3.tf
@@ -4,8 +4,10 @@ resource "aws_sqs_queue" "my_queue" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.terraform_bucket_name
+  region = var.aws_region
 }
