9f4a48ff5ec8061f168c1d4633a7d972b2bba12c
Raz Zinger <raz.roman-zinger@hp.com>
2025-03-19 20:09:30 +0200

    ff

--- terraform/vpc_network.tf
+++ terraform/vpc_network.tf
@@ -1,44 +1,55 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support    = true
-  enable_dns_hostnames  = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
+# Public Subnets in Different AZs
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.10.0/24"  # Changed to avoid conflicts
   map_public_ip_on_launch = true
   availability_zone       = var.aws_zone_1
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.20.0/24"  # Changed to avoid conflicts
   map_public_ip_on_launch = true
-  availability_zone       = var.aws_zone_1
+  availability_zone       = var.aws_zone_2 # Different AZ
 }
 
+# Private Subnet
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.1.1.0/24"
+  cidr_block        = "10.0.30.0/24"  # Changed to stay within VPC range
   availability_zone = var.aws_zone_1
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
+# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 }
 
+# Route to Internet Gateway
 resource "aws_route" "internet_access" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 }
 
-resource "aws_route_table_association" "public" {
+# Associate Public Subnets to Route Table
+resource "aws_route_table_association" "public_1" {
   subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
 }
