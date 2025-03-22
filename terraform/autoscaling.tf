2cc5b397244bdbbbe7f9ed81991df24c5a8a0aab
Raz Zinger <raz.roman-zinger@hp.com>
2025-03-23 00:37:19 +0200

    port mapping change in sg

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -1,40 +1,39 @@
 resource "aws_appautoscaling_target" "ecs_target" {
-  max_capacity       = 4
-  min_capacity       = 1
-  resource_id        = "service/${aws_ecs_cluster.my_cluster.name}/${aws_ecs_service.frontend.name}" 
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
+  max_capacity        = 4
+  min_capacity        = 1
+  resource_id         = "service/${aws_ecs_cluster.my_cluster.name}/${aws_ecs_service.frontend.name}"
+  scalable_dimension  = "ecs:service:DesiredCount"
+  service_namespace   = "ecs"
 }
 
 resource "aws_appautoscaling_policy" "ecs_scale_out" {
-  name               = "ecs-scale-out"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+  name                = "ecs-scale-out"
+  policy_type         = "TargetTrackingScaling"
+  resource_id         = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension  = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace   = aws_appautoscaling_target.ecs_target.service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = 75.0
-    predefined_metric_specification { predefined_metric_type = "ECSServiceAverageCPUUtilization" }
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
     scale_out_cooldown = 60
     scale_in_cooldown  = 120
   }
 }
 
-# Auto Scaling Policy - Scale In (Decrease Tasks)
 resource "aws_appautoscaling_policy" "ecs_scale_in" {
-    name               = "ecs-scale-in"
-    policy_type        = "TargetTrackingScaling"
-    resource_id        = aws_appautoscaling_target.ecs_target.resource_id
-    scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
-    service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+  name                = "ecs-scale-in"
+  policy_type         = "TargetTrackingScaling"
+  resource_id         = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension  = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace   = aws_appautoscaling_target.ecs_target.service_namespace
 
-    target_tracking_scaling_policy_configuration {
-        target_value = 30.0  # Scale down when CPU is below 30%
-            predefined_metric_specification {
-            predefined_metric_type = "ECSServiceAverageCPUUtilization"
-        }
-        scale_out_cooldown = 60
-        scale_in_cooldown  = 120
-    }
+  target_tracking_scaling_policy_configuration {
+    target_value = 30.0
+    scale_out_cooldown = 60
+    scale_in_cooldown  = 120
+    customized_metric_specification {}
+  }
 }
