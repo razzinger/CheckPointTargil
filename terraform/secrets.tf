c23535b7983acee774004d05698a0e2616851e01
Raz Zinger <raz.roman-zinger@hp.com>
2025-03-23 19:52:04 +0200

    fff

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,6 +1,6 @@
 # Create AWS Secrets Manager secret
-resource "aws_secretsmanager_secret" "sqs_s3_secrets_new" {
-  name = "sqs-s3-secrets"
+resource "aws_secretsmanager_secret" "sqs_s3_secrets" {
+  name = "sqs-s3-secrets_new"
 }
 
 resource "random_string" "token" {
@@ -10,7 +10,7 @@ resource "random_string" "token" {
 
 # Store values in the secret
 resource "aws_secretsmanager_secret_version" "sqs_s3_secret_value" {
-  secret_id     = aws_secretsmanager_secret.sqs_s3_secrets_new.id
+  secret_id     = aws_secretsmanager_secret.sqs_s3_secrets.id
   secret_string = jsonencode({
     VALID_TOKEN    = "${random_string.token.result}"
     SQS_QUEUE_URL  = aws_sqs_queue.my_queue.id
