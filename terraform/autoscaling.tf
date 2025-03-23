20a71abc7e3a5bd1581f1f4d7645317d46cb24df
Raz Zinger <raz.roman-zinger@hp.com>
2025-03-23 19:37:15 +0200

    fff

--- terraform/autoscaling.tf
+++ terraform/autoscaling.tf
@@ -6,34 +6,34 @@ resource "aws_appautoscaling_target" "ecs_target" {
   service_namespace   = "ecs"
 }
 
-resource "aws_appautoscaling_policy" "ecs_scale_out" {
-  name                = "ecs-scale-out"
-  policy_type         = "TargetTrackingScaling"
-  resource_id         = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension  = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace   = aws_appautoscaling_target.ecs_target.service_namespace
+# resource "aws_appautoscaling_policy" "ecs_scale_out" {
+#   name                = "ecs-scale-out"
+#   policy_type         = "TargetTrackingScaling"
+#   resource_id         = aws_appautoscaling_target.ecs_target.resource_id
+#   scalable_dimension  = aws_appautoscaling_target.ecs_target.scalable_dimension
+#   service_namespace   = aws_appautoscaling_target.ecs_target.service_namespace
 
-  target_tracking_scaling_policy_configuration {
-    target_value = 75.0
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization"
-    }
-    scale_out_cooldown = 60
-    scale_in_cooldown  = 120
-  }
-}
+#   target_tracking_scaling_policy_configuration {
+#     target_value = 75.0
+#     predefined_metric_specification {
+#       predefined_metric_type = "ECSServiceAverageCPUUtilization"
+#     }
+#     scale_out_cooldown = 60
+#     scale_in_cooldown  = 120
+#   }
+# }
 
-resource "aws_appautoscaling_policy" "ecs_scale_in" {
-  name                = "ecs-scale-in"
-  policy_type         = "TargetTrackingScaling"
-  resource_id         = aws_appautoscaling_target.ecs_target.resource_id
-  scalable_dimension  = aws_appautoscaling_target.ecs_target.scalable_dimension
-  service_namespace   = aws_appautoscaling_target.ecs_target.service_namespace
+# resource "aws_appautoscaling_policy" "ecs_scale_in" {
+#   name                = "ecs-scale-in"
+#   policy_type         = "TargetTrackingScaling"
+#   resource_id         = aws_appautoscaling_target.ecs_target.resource_id
+#   scalable_dimension  = aws_appautoscaling_target.ecs_target.scalable_dimension
+#   service_namespace   = aws_appautoscaling_target.ecs_target.service_namespace
 
-  target_tracking_scaling_policy_configuration {
-    target_value = 30.0
-    scale_out_cooldown = 60
-    scale_in_cooldown  = 120
-    customized_metric_specification {}
-  }
-}
+#   target_tracking_scaling_policy_configuration {
+#     target_value = 30.0
+#     scale_out_cooldown = 60
+#     scale_in_cooldown  = 120
+#     customized_metric_specification {}
+#   }
+# }
