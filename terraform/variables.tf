2a359f764b0f201ca97e725615e24594081fa4fc
Raz Zinger <raz.roman-zinger@hp.com>
2025-03-25 16:21:12 +0200

    gg

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,3 +22,9 @@ variable "bucket_name" {
   description = "S3 bucket for targil"
   default     = "chkpoint-targil-bucket"
 }
+
+variable "docker_hub_creds" {
+  description = "DockerHub Creds"
+  default     = "arn:aws:secretsmanager:eu-north-1:347161580074:secret:DockerHubCreds-rvY7Wo"
+}
+
--- terraform/vpc_network.tf
+++ terraform/vpc_network.tf
@@ -4,46 +4,69 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 }
 
-# Public Subnets in Different AZs
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.10.0/24"  # Changed to avoid conflicts
+  cidr_block              = "10.0.10.0/24"
   map_public_ip_on_launch = true
   availability_zone       = var.aws_zone_1
+  tags = {
+    Name = "public-01"
+  }
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.20.0/24"  # Changed to avoid conflicts
+  cidr_block              = "10.0.20.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = var.aws_zone_2 # Different AZ
+  availability_zone       = var.aws_zone_2
+  tags = {
+    Name = "public-02"
+  }
 }
 
-# Private Subnet
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.30.0/24"  # Changed to stay within VPC range
+  cidr_block        = "10.0.30.0/24"
   availability_zone = var.aws_zone_1
+  tags = {
+    Name = "private-02"
+  }
 }
 
-# Internet Gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
-# Public Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "public-route-table"
+  }
 }
 
-# Route to Internet Gateway
 resource "aws_route" "internet_access" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 }
 
-# Associate Public Subnets to Route Table
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_gateway" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public_1.id
+
+  tags = {
+    Name = "nat-gw"
+  }
+
+  depends_on = [aws_internet_gateway.gw]
+}
+
+# Associate Subnets to Route Table
 resource "aws_route_table_association" "public_1" {
   subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public.id
@@ -52,4 +75,21 @@ resource "aws_route_table_association" "public_1" {
 resource "aws_route_table_association" "public_2" {
   subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+# =======================================
+
+# Route for Private Subnet to NAT Gateway
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
 }
